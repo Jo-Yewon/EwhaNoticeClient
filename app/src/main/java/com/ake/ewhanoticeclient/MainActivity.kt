78983b7d14f045aab19bf46a5fad9080438dec26
QueenCurry <yeawonjo@gmail.com>
2019-12-29 23:59:36 +0900

    Initial commit

---
+++ app/src/main/java/com/ake/ewhanoticeclient/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ake.ewhanoticeclient
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
