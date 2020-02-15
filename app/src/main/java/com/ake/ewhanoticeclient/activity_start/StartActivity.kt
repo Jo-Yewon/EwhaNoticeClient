4ebd485a2a15d95cb2dc63e0a4c45be42c6444b1
QueenCurry <yeawonjo@gmail.com>
2020-02-15 20:56:34 +0900

    fix: 화생분 학과 DB 업데이트, 사회봉사팀 및 생명과학과 추가, v1.3.3
    - 기존 화학.나노과학과가 화생분으로 기록되어있던 것을 수정했습니다.
    - 사회봉사팀 및 생명과학과를 DB에 추가함
    - versionCode 7
    - versionName 1.3.3
    - DB version 5

--- app/src/main/java/com/ake/ewhanoticeclient/activity_start/StartActivity.kt
+++ app/src/main/java/com/ake/ewhanoticeclient/activity_start/StartActivity.kt
@@ -8,6 +8,7 @@ import androidx.databinding.DataBindingUtil
 import com.ake.ewhanoticeclient.R
 import com.ake.ewhanoticeclient.activity_main.MainActivity
 import com.ake.ewhanoticeclient.activity_subscribe.SubscribeActivity
+import com.ake.ewhanoticeclient.database.Board
 import com.ake.ewhanoticeclient.database.BoardDatabase
 import com.ake.ewhanoticeclient.database.BoardRepository
 import com.ake.ewhanoticeclient.databinding.ActivityStartBinding
@@ -20,13 +21,37 @@ class StartActivity : AppCompatActivity() {
 
         val dao = BoardDatabase.getInstance(application).BoardDatabaseDao
         val sharedPreferences = getSharedPreferences(
-            BoardRepository.PREFERENCES_NAME, Context.MODE_PRIVATE)
+            BoardRepository.PREFERENCES_NAME, Context.MODE_PRIVATE
+        )
         val repository = BoardRepository.getInstance(dao, sharedPreferences)
 
-        if (repository.getSubscribedBoardList().isEmpty()){
+        val subscribedBoards = repository.getSubscribedBoardList()
+
+        if (subscribedBoards.isEmpty()) {
             repository.setPushStatus(true)  // Default true
             startActivity(Intent(this, SubscribeActivity::class.java))
-        }else
+
+        } else {
+            // (update) 화학나노과학과의 게시판이 화생분으로 설정된 DB 수정 확인 과정
+            var needUpdate = false
+            val updatedSubscribedBoards = mutableListOf<Board>()
+            for (board in subscribedBoards) {
+                updatedSubscribedBoards.add(
+                    if (board.boardId == 2153220 && board.alias.equals("화생분공지")) {
+                        needUpdate = true
+                        Board(
+                            2153220, "Nature",
+                            "화학나노과학과공지사항", "화학나노과학공지")
+                    } else if (board.boardId == 2153221 && board.alias.equals("화생분채용")) {
+                        needUpdate = true
+                        Board(
+                            2153221, "Nature",
+                            "화학나노과학과채용정보", "화학나노과학채용")
+                    } else board
+                )
+            }
+            if (needUpdate) repository.setSubscribedBoardList(updatedSubscribedBoards)
             startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
